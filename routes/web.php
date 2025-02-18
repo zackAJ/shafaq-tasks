4fa3413f87e3bbcf9de89e040bc1f1d55158ac1e
zackaj <aktouf98zakaria@gmail.com>
2025-02-18 19:28:46 +0100

    drop the elbow from the top rope

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,13 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return ['Laravel' => app()->version()];
+    Artisan::call('inspire');
+    $quote = Artisan::output();
+
+    return "<body style='background-color:black; color:white;'>{$quote}</body>";
 });
 
 require __DIR__.'/auth.php';
